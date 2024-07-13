889b10de2bc5a54b7d8ef7041bc26f8f0d1bb4db
20KimMinki24 <virizion1228@naver.com>
2024-07-13 15:51:53 +0900

    아두이노 자율주행차 5차
    코드를 ChatGPT에게 맡기는 것보다는 사람이 직접 탐구하면서 수정해야 한다. ChatGPT 코드를 그대로 퍼왔는데 계속 오류가 나서 내가 직접 나섰더니 이제 라인트레이서 10바퀴를 돌았는데 이탈을 한번도 안함. 대박. 이제 굴절코스 등을 실험해야 한다.

---
+++ ArduinoSelfDrivingCarCode20240713.ino
@@ -0,0 +1,86 @@
+// 모터 드라이버 핀 정의
+#define ENA 9
+#define ENB 10
+#define IN1 11
+#define IN2 12
+#define IN3 7
+#define IN4 8
+
+// 라인트레이서 센서 핀 정의
+#define LEFT_SENSOR_PIN A0
+#define LSP A1
+#define RSP A3
+#define RIGHT_SENSOR_PIN A4
+
+// 속도 정의
+#define V 38
+
+
+void setup() {
+  // 모터 드라이버 핀을 출력 모드로 설정
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  // 센서 핀을 입력 모드로 설정
+  pinMode(LEFT_SENSOR_PIN, INPUT);
+  pinMode(LSP, INPUT);
+  pinMode(RSP, INPUT);
+  pinMode(RIGHT_SENSOR_PIN, INPUT);
+}
+
+void loop() {
+  // 각 센서 값 읽기
+  int leftSensor = digitalRead(LEFT_SENSOR_PIN);
+  int leftCenterSensor = digitalRead(LSP);
+  int rightCenterSensor = digitalRead(RSP);
+  int rightSensor = digitalRead(RIGHT_SENSOR_PIN);
+
+  // 센서 값을 바탕으로 모터 제어
+  if (leftSensor == LOW && rightSensor == LOW) {
+    moveForward();
+  } else if (leftSensor == LOW || leftCenterSensor==LOW){
+    turnLeft();
+  } else if (rightSensor == LOW || rightCenterSensor==LOW) {
+    turnRight();
+  } else {
+    moveForward();  // 라인을 감지하지 못하면 직진
+  }
+}
+
+void moveForward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, V);
+  analogWrite(ENB, V);
+}
+
+void turnLeft() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, V);
+  analogWrite(ENB, V);
+}
+
+void turnRight() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENA, V);
+  analogWrite(ENB, V);
+}
+
+void stopCar() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
